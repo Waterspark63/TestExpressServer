0e898eda771f34d30b20934ed6fcc0c5476af48f
Moonlit Eclipse <varxs.kids2@gmail.com>
2026-01-19 03:58:06 -0800

    bob3

--- react-web/src/api/axiosClient.js
+++ react-web/src/api/axiosClient.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const axiosClient = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000",
+    baseURL: process.env.REACT_APP_API_URL || "https://testexpressserver.onrender.com/",
     headers: { "Content-Type": "application/json" },
 });
 // insert JWT token if available
